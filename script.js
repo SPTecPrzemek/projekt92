5095d72472ed55a06293724f94519430d57ffa88
SPTecPrzemek <stasiak.przemyslaw@wp.pl>
2017-04-03 00:04:29 +0200

    done

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 /**
  * Created by stasiakprzemyslaw on 13.03.2017.
  */
-$(document).ready(function(){
-    console.log('DOM załadowany - można się bawić');
+
+$(function () {
     var span = $("span");
-    span.each(function (index,element) {
-        if (index %2 == 0) {
-            $(element).css('color','red');
-        }
+    span.each(function(index, element) {
+        if(index % 2 == 0) {
+            $(element).css('color', 'red');
+        };
     });
 
     var paragraphs = $('p');
-    paragraphs.each(function (index,element) {
-        var button = '<button class="btn data-tmp="' + index + '">Click me </button>';
-        $(element).append(button);
-        });
-    
-    $("button").click(function () {
-        alert($(this).attr("data-tmp"));
+    paragraphs.each(function(index, element) {
+        var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+        $(element).append(button)
+    });
 
+    $("button").click(function(){
+        alert($(this).attr("data-tmp"));
     });
 });
+
 
 
 
