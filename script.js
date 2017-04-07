e1ec9015ffa1cfd9669040bda2d697444d2204be
SPTecPrzemek <stasiak.przemyslaw@wp.pl>
2017-04-08 00:29:26 +0200

    done

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ $(function () {
     var paragraphs = $('p');
     paragraphs.each(function(index, element) {
         var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
-        $(element).append(button)
+        $(element).append(button);
     });
 
     $("button").click(function(){
